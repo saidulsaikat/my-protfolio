7d33e79b9599862d6f4c6a3a658320d771ab8b20
saidulhasan <saidulhasan562@gmail.com>
2024-03-03 03:29:17 +0600

    add skill section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Skills from "./components/Skills";
 
 function App() {
   return <>
   <Navbar />
   <Hero />
+  <Skills />
   </>
 }
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,9 +5,9 @@ import { FaGithubSquare, FaLinkedin, FaTwitterSquare } from "react-icons/fa";
 const Hero = () => {
   return (
     <div className=" bg-emerald-100 py-24">
-      <div className=" max-w-7xl mx-auto px-8 grid md:grid-cols-2 items-center gap-8">
+      <div className="align-element grid md:grid-cols-2 items-center gap-8">
         <article>
-          <h1 className=" text-7xl font-bold tracking-wider">I'm Saikat</h1>
+          <h1 className=" text-7xl font-bold tracking-wider">I'm Saidul</h1>
           <p className=" mt-4 text-3xl text-slate-700 capitalize tracking-wide">
             Front-End Developer
           </p>
@@ -18,7 +18,7 @@ const Hero = () => {
             <a href="https://github.com/saidulsaikat">
               <FaGithubSquare className=" h-8 w-8 text-slate-500 hover:text-black duration-300" />
             </a>
-            <a href="#">
+            <a href="https://www.linkedin.com/in/saidul-hasan/">
               <FaLinkedin className=" h-8 w-8 text-slate-500 hover:text-black duration-300" />
             </a>
             <a href="#">
